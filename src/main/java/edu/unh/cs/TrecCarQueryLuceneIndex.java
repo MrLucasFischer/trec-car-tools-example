ac82fb7d1d41a9f862dbc1ad689e70264932a398
MrLucasFischer <lucasfischerpt@gmail.com>
2019-01-21 17:50:26 +0000

    updates

--- src/main/java/edu/unh/cs/TrecCarQueryLuceneIndex.java
+++ src/main/java/edu/unh/cs/TrecCarQueryLuceneIndex.java
@@ -199,7 +199,7 @@ public class TrecCarQueryLuceneIndex {
             searcher.setSimilarity(new BM25Similarity());
             final MyQueryBuilder queryBuilder = new MyQueryBuilder(new StandardAnalyzer());
 
-            try (BufferedReader br = new BufferedReader(new FileReader("data/benchmarkY2.titles"))) {
+            try (BufferedReader br = new BufferedReader(new FileReader("./data/benchmarkY2.titles"))) {
                 String line = br.readLine();
 
 //                TODO to iterate over all lines
